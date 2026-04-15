1f97f5af93fbf5adb3c9658c304ea7419d038ad1
danielG-blip <jgiraldo1@cesde.net>
2026-04-15 16:34:14 -0500

    Creacion proyecto

---
+++ src/main/java/com/tienda/luking_store/LukingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.luking_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukingStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/luking_store/LukingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.luking_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
